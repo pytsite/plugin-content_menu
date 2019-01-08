a68000b01c9481c625350e70f2c67b2c6777b286
Oleksandr Shepetko <a@shepetko.com>
2019-01-08 23:35:26 +0200

    Bugfix 1.0.1

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 ## Changelog
 
 
+### 1.0.1 (2019-01-08)
+
+Missing export fixed.
+
+
 ### 1.0 (2019-01-07)
 
 First release.
--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ __author__ = 'Oleksandr Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+from ._model import ContentMenu
+
 
 def plugin_load():
     from pytsite import reg
@@ -12,4 +14,3 @@ def plugin_load():
 
     if reg.get('content_menu.register_default_model', True):
         menu.register_model('content_menu', _model.ContentMenu, __name__ + '@content')
-
